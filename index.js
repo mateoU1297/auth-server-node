44364263b817c0f65a3d33f9f2c2ef1c4f73cc06
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-02-28 14:52:02 -0500

    se agrega front a la carpeta public y se implementa middleware para rutear front correctamente

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
@@ -13,6 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
+
+app.get('*', (req, res) => {
+  res.sendFile( path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`servidor corriendo en el puerto ${process.env.PORT}`);
